66a6145890874492d1af28760add918fe9771f98
Dev Singh <devksingh4@gmail.com>
2018-07-22 09:52:48 -0500

    Update credentials.js

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,7 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
-  GITHUB_CALLBACK_URL: '',
-  DBURL: '',
+  GITHUB_CLIENT_ID: '0b4eb8b1a077e3e62852',
+  GITHUB_CLIENT_SECRET: process.env.clientSecret,
+  GITHUB_CALLBACK_URL: process.env.callbackURL,
+  DBURL: 'mongodb://hackchicago18:hackchicago18@ds147411.mlab.com:47411/reforum',
 };
+
