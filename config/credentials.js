e7e12d1d63933915f1debb60e04316647b76a79c
Dev Singh <devksingh4@gmail.com>
2018-07-22 09:59:37 -0500

    Update credentials.js

--- config/credentials.js
+++ config/credentials.js
@@ -1,7 +1,7 @@
 module.exports = {
   GITHUB_CLIENT_ID: '0b4eb8b1a077e3e62852',
-  GITHUB_CLIENT_SECRET: process.env.clientSecret,
-  GITHUB_CALLBACK_URL: process.env.callbackURL,
+  GITHUB_CLIENT_SECRET: "af5378f4608b879ea3d363624ffd5f4aa49a0106",
+  GITHUB_CALLBACK_URL: "http://forum.icedtea.tech/api/user/authViaGitHub/callback",
   DBURL: 'mongodb://hackchicago18:hackchicago18@ds147411.mlab.com:47411/reforum',
 };
 
